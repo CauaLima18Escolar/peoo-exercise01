d598e1aa9a2b57ee52a17c88bad867f31066a77a
Cauã de Lima <caua.lima@escolar.ifrn.edu.br>
2024-08-25 12:46:28 -0300

    Update question14.js

--- Js/question14.js
+++ Js/question14.js
@@ -4,6 +4,29 @@ todos os números contidos nesse intervalo. Por exemplo:
 expandRange("1-3,5,7-9"); // retorna [1, 2, 3, 5, 7, 8, 9] */
 
 function expandRange(str){
-    str = str.split(',')
-    
-}
+
+    if (str.length === 0 || typeof str !== 'string'){
+        return null
+    }
+
+    const arrResult = [];
+    let strParts = str.split(',');
+
+    for(let part of strParts){
+        if (part.includes('-')){
+            let [numStart, numEnd] = part.split('-');
+            numStart = Number(numStart);
+            numEnd = Number(numEnd);
+
+            for(; numStart <= numEnd; numStart++){
+                arrResult.push(numStart)
+            };
+        } else {
+            arrResult.push(Number(part));
+        };
+    };
+
+    return arrResult
+ };
+
+ console.log(expandRange('1-5, 6, 7-12, 13, 14-30'));
